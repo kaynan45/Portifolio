65d3b151de521ffd3bdfc2589d9445b83d48cd66
Lorenzo Kaynan Xavier <lorenzokaynanxavier@gmail.com>
2024-03-05 07:38:00 -0300

    Trying to add the post input

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,14 +1,21 @@
 import { posts } from "./data.js";
 
-let postHtml;
-posts.forEach((post) => {
-  postHtml += `
+const postInput = document.querySelector(".post-input");
+const newPostButton = document.querySelector(".add-post-button");
+
+newPostButton.addEventListener("click", () => {
+
+  
+  let postHtml = "";
+  posts.forEach((post) => {
+    postHtml += `
     <div class="post">
         <h3 class="post-title">${post.title}</h3>
         <p class="post-content"> ${post.content} </p>
         <p class="post-author">${post.author}</p>
     </div>
     `;
-});
+  });
 
-document.querySelector(".posts-grid").innerHTML = postHtml
+  document.querySelector(".posts-grid").innerHTML = postHtml;
+});
