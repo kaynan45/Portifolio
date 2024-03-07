ddb51ceef7cc38e1493b339bdc69bc19231678cd
Lorenzo Kaynan Xavier <lorenzokaynanxavier@gmail.com>
2024-03-07 18:15:18 -0300

    implement like and comment HTML buttons

--- javaScript/script.js
+++ javaScript/script.js
@@ -18,7 +18,19 @@ const newPostButton = document.querySelector(".add-post-button");
 const postsArea = document.querySelector(".posts-grid");
 
 newPostButton.addEventListener("click", () => {
-  postsArea.insertAdjacentHTML("afterbegin", `<li>${postInput.value}</li>`);
+  postsArea.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="entirePost js-entire-post">
+    <li class="postContent">${postInput.value}</li>
+    <div class="post-actions">
+    <button class="like-button js-like-button"><i class="fas fa-thumbs-up"></i></button>
+    <button class="comment-button js-comment-button"><i class="fas fa-comment"></i></button>
+</div>
+<div class="comments-section js-comments-section">
+    <!-- Comments will be added dynamically here -->
+</div>
+</div>`
+  );
 
   miniGold.posts.push({
     id: miniGold.posts.length + 1,
@@ -28,3 +40,7 @@ newPostButton.addEventListener("click", () => {
   console.log(miniGold.posts);
   postInput.value = "";
 });
+
+function readPosts() {
+  return miniGold.posts;
+}
