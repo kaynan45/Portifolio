e08724a15ce47a1a1bd21c1ae614d93578dc8b5d
Lorenzo Kaynan Xavier <lorenzokaynanxavier@gmail.com>
2024-03-12 08:49:31 -0300

    Making it work

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,46 +1,10 @@
-const miniGold = {
-  users: [
-    {
-      username: "kaynan45",
-    },
-  ],
-  posts: [
-    {
-      id: 1,
-      owner: "kaynan45",
-      content: "First goldy",
-    },
-  ],
+
+document.
+
+const closeModal = () => {
+  modal.classList.remove("active")
 };
 
-const postInput = document.querySelector(".post-input");
-const newPostButton = document.querySelector(".add-post-button");
-const postsArea = document.querySelector(".posts-grid");
-
-newPostButton.addEventListener("click", () => {
-  postsArea.insertAdjacentHTML(
-    "afterbegin",
-    `<div class="entirePost js-entire-post">
-    <li class="postContent">${postInput.value}</li>
-    <div class="post-actions">
-    <button class="like-button js-like-button"><i class="fas fa-thumbs-up"></i></button>
-    <button class="comment-button js-comment-button"><i class="fas fa-comment"></i></button>
-</div>
-<div class="comments-section js-comments-section">
-    <!-- Comments will be added dynamically here -->
-</div>
-</div>`
-  );
-
-  miniGold.posts.push({
-    id: miniGold.posts.length + 1,
-    owner: "kaynan45",
-    content: postInput.value,
-  });
-  console.log(miniGold.posts);
-  postInput.value = "";
-});
-
-function readPosts() {
-  return miniGold.posts;
-}
+const openModal = () => {
+  modal.classList.add("active")
+};
