f2e48148d3a59c9623fc74ccdd0c685b307e881d
Lorenzo Kaynan Xavier <lorenzokaynanxavier@gmail.com>
2024-09-15 13:17:09 -0300

    Getting the navbar to work

--- Book-tracker/src/App.js
+++ Book-tracker/src/App.js
@@ -1,19 +1,23 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/pages/Home';
-import AddBooks from './components/pages/AddBooks';
-import BookList from './components/pages/BookList';
-import Navbar from './components/layout/Navbar';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import AddBooks from "./components/pages/AddBooks";
+import BookList from "./components/pages/BookList";
+import Navbar from "./components/layout/Navbar";
+import Container from "./components/layout/Container";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/add-books' element={<AddBooks />} />
-        <Route path='/book-list' element={<BookList />} />
-      </Routes>
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add-books" element={<AddBooks />} />
+          <Route path="/book-list" element={<BookList />} />
+        </Routes>
+      </Container>
+      <p>Footer</p>
     </Router>
   );
 }
--- Book-tracker/src/components/layout/Navbar.js
+++ Book-tracker/src/components/layout/Navbar.js
@@ -1,19 +1,31 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+
+import styles from "./Navbar.module.css";
+import Container from "./Container";
+
+import logo from "../../img/book.png";
 
 function Navbar() {
   return (
-    <ul>
-      <li>
-      <Link to='/'>Home</Link>
-      </li>
-      <li>
-      <Link to='/add-books'>Add books</Link>
-      </li>
-      <li>
-      <Link to='/book-list'>Home</Link>
-      </li>
-    </ul>
-  )
+    <nav>
+      <Container>
+        <ul>
+          <li>
+            <img className="logo" src={logo} />
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/add-books">Add books</Link>
+          </li>
+          <li>
+            <Link to="/book-list">Home</Link>
+          </li>
+        </ul>
+      </Container>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
