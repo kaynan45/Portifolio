751d6f74b42ebc33d55e366269547838b9c1633b
Lorenzo Kaynan Xavier <lorenzokaynanxavier@gmail.com>
2024-09-15 18:38:49 -0300

    Making the "add books" page

--- Book-tracker/src/components/pages/AddBooks.js
+++ Book-tracker/src/components/pages/AddBooks.js
@@ -1,7 +1,27 @@
+import styles from "./AddBooks.module.css";
+
 function AddBooks() {
+  function addBook(e) {
+    e.preventDefault();
+    alert("book added!");
+  }
+
   return (
-    <h1>AddBooks content</h1>
-  )
+    <section className={styles.bookSection}>
+      <h1>Add your books!</h1>
+      <form className={styles.booksForm}>
+        <div>
+          <label>Book name</label>
+          <input type="text" className={styles.bookInputs}/>
+        </div>
+        <div>
+          <label>Book author</label>
+          <input type="text" className={styles.bookInputs}/>
+        </div>
+        <input className={styles.addBook} value="Add Book" type="submit" onClick={addBook} />
+      </form>
+    </section>
+  );
 }
 
-export default AddBooks
+export default AddBooks;
