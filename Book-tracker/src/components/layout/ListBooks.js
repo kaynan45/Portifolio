680575b9092b2c1a619b65d56d8f3c426e52bebd
Lorenzo Kaynan Xavier <lorenzokaynanxavier@gmail.com>
2024-09-21 19:45:33 -0300

    Starting to try show the book cover using 2 APIs

--- Book-tracker/src/components/layout/ListBooks.js
+++ Book-tracker/src/components/layout/ListBooks.js
@@ -1,11 +1,11 @@
 import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 import styles from "./ListBooks.module.css";
 
 import Container from "../layout/Container";
 import Button from "./Button";
 import Message from "./Message";
-
 
 function ListBooks({ book }) {
   const location = useLocation();
@@ -16,19 +16,33 @@ function ListBooks({ book }) {
     message = location.state.message;
   }
 
+  const [coverId, setCoverId] = useState();
+
+  useEffect(() => {
+    fetch(
+      `https://openlibrary.org/search.json?title=${book.name}&author=${book.author}`
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error))
+  }, [book]);
+
   return (
     <div className={styles.bookContainer}>
       <div className={styles.titleContainer}>
         <h1>My tracked books</h1>
         <Button buttonText="Add a new book!" buttonPath="../add-books" />
       </div>
-      {message && <Message messageText={message} /> }
+      {message && <Message messageText={message} />}
       <Container customClass="start">
         <ul className={styles.books}>
           {book.map((book) => (
             <li className={styles.book} key={book.id}>
               <span>
-                <img src="https://via.placeholder.com/200" />
+                {/* Because that is a public url, we don`t have to fetch it, we can just provide the url as a src and boom */}
+                <img
+                  src={`https://covers.openlibrary.org/b/isbn/${book.isbn}-M.jpg`}
+                />
               </span>
               <h3>{book.name}</h3> <p>{book.author}</p>
             </li>
