b632c89c2e36db3ce4a3c166f6e031193277518b
Lorenzo Kaynan Xavier <lorenzokaynanxavier@gmail.com>
2024-09-22 16:50:22 -0300

    Automatic books covers implemented

--- Book-tracker/src/components/layout/ListBooks.js
+++ Book-tracker/src/components/layout/ListBooks.js
@@ -16,15 +16,27 @@ function ListBooks({ book }) {
     message = location.state.message;
   }
 
-  const [coverId, setCoverId] = useState();
+  const [covers, setCovers] = useState({});
 
   useEffect(() => {
-    fetch(
-      `https://openlibrary.org/search.json?title=${book.name}&author=${book.author}`
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error))
+    book.forEach((singleBook) => { //? Braking the book array into singleBooks objects, doing that we can...
+      fetch(
+        `https://openlibrary.org/search.json?title=${singleBook.name}&author=${singleBook.author}` // Use the created singleBook object to get the name and the author of the books
+      )
+        .then((response) => response.json())
+        .then((data) => { //data provided by the API
+          if (data.docs && data.docs.length > 0) { //if the data docs exists...
+            const coverId = data.docs[0].cover_i; // Allocate it`s value into a variable named coverId
+            if (coverId) { //If coverId have some value inside
+              setCovers((prevCovers) => ({ // `prevCovers` is a reference to the previous state of `covers`
+                ...prevCovers, // Spreading the object to maintain the previous data of it
+                [singleBook.id]: coverId, // Dynamically creating a key (being each book id) and a value that is the cover id provided by the API
+              }));
+            }
+          }
+        })
+        .catch((error) => console.log(error));
+    });
   }, [book]);
 
   return (
@@ -36,15 +48,18 @@ function ListBooks({ book }) {
       {message && <Message messageText={message} />}
       <Container customClass="start">
         <ul className={styles.books}>
-          {book.map((book) => (
+          {book.map((singleBook) => (
             <li className={styles.book} key={book.id}>
               <span>
                 {/* Because that is a public url, we don`t have to fetch it, we can just provide the url as a src and boom */}
                 <img
-                  src={`https://covers.openlibrary.org/b/isbn/${book.isbn}-M.jpg`}
+                  src={covers[singleBook.id]
+                    ? `https://covers.openlibrary.org/b/id/${covers[singleBook.id]}-M.jpg`
+                    : "https://via.placeholder.com/200"
+                  }
                 />
               </span>
-              <h3>{book.name}</h3> <p>{book.author}</p>
+              <h3>{singleBook.name}</h3> <p>{singleBook.author}</p>
             </li>
           ))}
         </ul>
