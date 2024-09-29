f3f1c6424a9e5549cac75c9f8a0b246bb8cab853
Lorenzo Kaynan Xavier <lorenzokaynanxavier@gmail.com>
2024-09-29 09:40:56 -0300

    fixed the books display

--- Book-tracker/src/components/layout/Container.js
+++ Book-tracker/src/components/layout/Container.js
@@ -1,9 +1,9 @@
 import styles from "./Container.module.css";
 
-function Container(props) {
+function Container({customClass, children}) {
   return (
-    <div className={`${styles.container} ${styles[props.customClass]}`}>
-      {props.children}
+    <div className={`${styles.container} ${styles[customClass]}`}>
+      {children}
     </div>
   );
 }
